8eed6e3f12aa691f8a6962221cbcc8615171e63a
Farhan Ali Raza <farhanalirazaazeemi@gmail.com>
2022-08-23 10:42:44 +0500

    done final

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,28 @@
     import "../app.css";
     //some test
   </script>
-  
+  <svelte:head>
+    <title>Al Quran - Qari Abdul Basit With Urdu transalation - Listen Online - Surah Rehman Qari Abdul Basit - PWA - Works Offline</title>
+    <meta name="description" content="Full quran audio in voice of qari abdul basit with urdu translation">
+    <meta name="theme-color" content="#000">
+  <link rel="manifest" href="/manifest.webmanifest">
+  <link rel="shortcut icon" type="image/svg" href="/svelte.svg">
+  <link rel="apple-touch-icon" href="/svelte.svg">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="My Apps">
+  <meta name="application-name" content="My Apps">
+  <meta name="mobile-web-app-capable" content="yes">
+  <!--  Essential META Tags -->
+  <meta property="og:title" content="Al Quran - Qari Abdul Basit With Urdu transalation - PWA - Works Offline ">
+  <meta property="og:type" content="article" />
+  <meta property="og:image" content="https://alquranlisten.live/logo.png">
+  <meta property="og:url" content="https://alquranlisten.live/">
+  <meta name="twitter:card" content="summary_large_image">
+
+  <!--  Non-Essential, But Recommended -->
+  <meta property="og:description" content="Offering tour packages for individuals or groups.">
+  <meta property="og:site_name" content="European Travel, Inc.">
+  <meta name="twitter:image:alt" content="Alt text for image">
+  </svelte:head>
   <slot />
